d07cbbfb5bab498b60b9c86a8f72273e2ea788d7
D34dMan <shibin@factorial.io>
2020-04-04 15:56:05 +0200

    Add support for list_predefined_options plugin_id to be injected for relevant fields

--- src/transformers/Utils/FieldStorage.php
+++ src/transformers/Utils/FieldStorage.php
@@ -26,10 +26,20 @@ class FieldStorage extends FieldBase
 
     protected function getTemplateOverrideData()
     {
-        return [
+        $out = [
             'uuid' => PlaceholderService::REUSE_OR_CREATE_VALUE,
             'id' => $this->entity_type . '.' . $this->getFieldName(),
             'field_name' => $this->getFieldName(),
         ];
+        if ($this->usesListPredefinedOptions()) {
+            $plugin_id = $this->data['plugin_id'] ?? 'us_states';
+            $out['third_party_settings']['list_predefined_options']['plugin_id'] = $plugin_id;
+        }
+        return $out;
+    }
+
+    protected function usesListPredefinedOptions() {
+        $modules = $this->template['dependencies']['module'] ?? [];
+        return in_array('list_predefined_options', $modules);
     }
 }
